ec7e67f25f593638b2602f9ba6136ca78d4409ca
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-09-15 09:35:45 -0300

    Att

---
+++ aula.js
@@ -0,0 +1,14 @@
+alert('Ola mundo')
+/* Comentario */
+// comentario
+document.write('Olaaaaaa');
+
+console.log('Usando o console');   
+
+let h1 = window.document.getElementsByName('h1')
+
+let mensagem = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+
+document.write(mensagem)
+
+console.log(mensagem)
