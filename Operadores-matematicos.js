46c06991d463fd20f08048bb2b263c3670df6cfe
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-09-22 09:35:39 -0300

    commit

---
+++ Operadores-matematicos.js
@@ -0,0 +1,22 @@
+let n1 = Number(10);
+let n2 = Number(5);
+
+
+let soma = n1 + n2
+let sub = n1 - n2
+let mult = n1*n2
+let pot = n1**n2
+let div = n1/n2
+let rest = n1%n2
+console.log("O resultado = " + soma); //Soma
+
+console.log("O resultado = " + sub); //Subtração
+
+console.log("O resultado = " + mult); //Multiplicação
+
+console.log("O resultado = " + pot); //Potenciação
+
+console.log("O resultado = " + div);//Divisão
+
+console.log("O resultado = " + rest); //Resto
+
--- manipulando-texto-numeros.js
+++ manipulando-texto-numeros.js
@@ -1,12 +1,21 @@
 let nomeAluno1 = 'Pedro Henrique';
 let txt = 'Estão Chegando as provas';
+let num = 123.123;
 
 console.log(nomeAluno1.length); //Conta quantos espaçõs tem na variavel
 
 console.log(txt.indexOf("as")); //Acha em que posição esta (Acha a primeira ocorrencia)
 
-console.log(txt.lastIndexOf("provas")) //Acha a ultima ocorrencia da palavra
+console.log(txt.lastIndexOf("provas")); //Acha a ultima ocorrencia da palavra
 
-console.log(txt.slice(0,5)) //Ira recortar uma parte da string (do index 0 ao 5)
+console.log(txt.slice(0,5)); //Ira recortar uma parte da string (do index 0 ao 5)
 
-console.log(txt.substr(6,8)) //Ira recortar a string também (do index 6 a 8)
+console.log(txt.substr(6,8)); //Ira recortar a string também (do index 6 a 8)
+
+console.log(txt.replace("provas","Avaliações"));  //A função vai mudar a palavra provas por Avaliações
+
+console.log(txt.toUpperCase()) //A função Vai mudar a palacra para letras maiusculas
+
+console.log(txt.toLowerCase()) //A função muda as letras para minúsculas
+
+console.log(num.toFixed(2))  //Define o nunero de casas decimais
