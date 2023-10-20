b5b31642e808e82bec03c1563b7f33bc7911038f
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-10-20 09:53:08 -0300

    commit

---
+++ array-exemplo.js
@@ -0,0 +1,7 @@
+const times = ["framengo", "paulista", "Botafogo"]
+
+console.table(times)
+
+times.push("Corinthians")
+
+console.table(times)
---
+++ exercicio-casa.js
@@ -0,0 +1,8 @@
+const cities = ["são paulo", "rio de janeiro", "minas gerais"];
+let upperCaseCities = [];
+
+for (city of cities){
+    upperCaseCities.push(city.toUpperCase())
+}
+console.log(cities)
+console.log(upperCaseCities);
